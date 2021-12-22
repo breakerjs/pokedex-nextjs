db89f2fd28db079fee6ff364d33ec0d96ab376b6
Franco Piccirilli <francopiccirilli51@gmail.com>
2021-12-22 00:18:57 -0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <footer className="footer py-3 bg-light">
+        <div className="container" id="footerWebPage">
+          <span className="text-muted">Proyecto de NextJS realizado por Franco Piccirilli.</span>
+        </div>
+       </footer>
+    )
+}
